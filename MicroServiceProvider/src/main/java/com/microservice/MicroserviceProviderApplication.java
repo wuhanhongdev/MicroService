a3f641b650c34316856c34fe68dac6002d52ffa9
wuhanhong <wuhanhong@jinhuhang.com.cn>
2017-10-17 14:42:04 +0800

    init provider project

---
+++ MicroServiceProvider/src/main/java/com/microservice/MicroserviceProviderApplication.java
@@ -0,0 +1,12 @@
+package com.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceProviderApplication.class, args);
+	}
+}
